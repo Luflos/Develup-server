680453d0e0dfed0260a2e0adae745b56e730ba46
Sol Youn <syoun93@gmail.com>
2022-04-01 09:44:19 -0700

    prep for deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 2996
 
 // middlewares
 app.use(cors())
